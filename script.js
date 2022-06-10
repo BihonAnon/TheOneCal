c911a202e5582a9fda96dadf5bce61e3df47170d
AhayahLit <lau1087054@gmail.com>
2022-06-10 13:31:38 -0700

    edited ids

--- script.js
+++
@@ -1,9 +0,0 @@
-$.ajax({
-    URL: "./api/getWeather",
-    data: {
-        zipcode: 90640;
-    },
-    success: function(result) {
-        $("weather.com  ").html ("<strong>" + result + "</strong> degrees");
-    }
-})
