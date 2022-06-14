a7a1e1475ce38aacdbaefbd1eeee1755f50ffd2b
OzerK <ozerkerr@gmail.com>
2022-06-14 07:54:23 -0700

    fix api

--- assets/js/apis.js
+++ assets/js/apis.js
@@ -1,37 +1,62 @@
 let arrayOfTemp = [];
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      let lat = position.coords.latitude;
-      let long = position.coords.longitude;
+let temp = 0;
+let city = "";
+let iconcode;
+let iconUrl;
+// function getLocation() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function (position) {
+//       let lat = position.coords.latitude;
+//       let long = position.coords.longitude;
 
-      fetchaApi(lat, long)
-    });
-  }
-}
 
-getLocation();
+//     });
+//   }
+// }
+let lat;
+let long;
+fetchaApi(lat, long)
 
 function fetchaApi(lat, long) {
-  if (lat) {
-    const settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": `https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=${lat}&lon=-${long}`,
-      "method": "GET",
-      "headers": {
-        "X-RapidAPI-Key": "8cd89d2ee5mshc3539f913d69aefp1b4693jsn3c2e1079babc",
-        "X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com"
-      }
-    };
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '8cd89d2ee5mshc3539f913d69aefp1b4693jsn3c2e1079babc',
+      'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com'
+    }
+  };
 
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-      console.log('www')
-      for (i = 0; i < response.data.length - 33; i++) {
-        arrayOfTemp.push(response.data[i].temp)
-        console.log(arrayOfTemp)
-      }
-    });
-  }
+  fetch('https://community-open-weather-map.p.rapidapi.com/weather?q=Los%20Angeles&units=imperial', options)
+    .then(response => response.json())
+    .then(function (data) {
+      temp = data.main.temp;
+      city = data.name;
+      iconcode = data.weather[0].icon
+      iconUrl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+
+      displayWeather();
+    })
+    .catch(err => console.error(err));
+}
+
+function displayWeather() {
+  let monday = $('#monday')
+  let firstDiv = $('#first-day')
+  let weatherDiv = $('<div>')
+  let spanEl = $('<span>')
+  let iconEl = $('<img>')
+
+  firstDiv.attr("class", "card-body col-sm-12 col-md-6")
+  weatherDiv.attr("class", "container col-sm-12 col-md-6 solution_card")
+  monday.css("display", "flex")
+  monday.css("flex-direction", "row")
+  spanEl.attr("class", "wheather-city")
+  iconEl.attr("src", iconUrl)
+  iconEl.attr("class", "icoon")
+
+  console.log(iconUrl)
+  spanEl.append(city + "      " + temp + " ")
+
+  weatherDiv.append(spanEl, iconEl)
+  monday.append(weatherDiv)
 }
